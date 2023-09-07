73adf595350f24e49c36200681fa37b9bcb9ce96
skyshake <133776304+skyshake@users.noreply.github.com>
2023-09-07 11:43:55 +0000

    Deploying to gh-pages from @ skyshake/team-project-team-6@4fb06d38365843e261af3b8e1d8e60f3d442452f 🚀

---
+++ index.5ff9eed4.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-buynow-2-open]"),closeModalBtn:document.querySelector("[data-buynow-2-close]"),modal:document.querySelector("[data-buynow]")};function o(){e.modal.classList.toggle("hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.5ff9eed4.js.map
---
+++ index.720a85b2.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-buynow-2-open]"),closeModalBtn:document.querySelector("[data-buynow-2-close]"),modal:document.querySelector("[data-buynow]")};function o(){e.modal.classList.toggle("hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.720a85b2.js.map
