12c82567cbce5717eacc16233a916887ce2dcc7e
skyshake <133776304+skyshake@users.noreply.github.com>
2023-09-07 03:52:37 +0000

    Deploying to gh-pages from @ skyshake/team-project-team-6@bd6fd988c36e82fe38bdd7a78260b7ed0d2b892d 🚀

--- index.ee03bcce.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-review-open]"),closeModalBtn:document.querySelector("[data-review-close]"),modal:document.querySelector("[data-review]")};function o(){e.modal.classList.toggle("hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.ee03bcce.js.map
