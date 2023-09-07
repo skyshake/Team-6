7bf69a214af7d8910ed1340a968afdbb1f0b61ec
AlgieRalph <algieralph@gmail.com>
2023-09-07 10:29:09 +0800

    reviewmodal1

---
+++ src/js/review.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-review-open]'),
+    closeModalBtn: document.querySelector('[data-review-close]'),
+    modal: document.querySelector('[data-review]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('hidden');
+  }
+})();
